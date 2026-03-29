7df4b3a25c203c0b1f861a2efd1f23ea34cadd6f
jiangjie <1121271124@qq.com>
2026-03-29 16:39:46 +0800

    修改tool
    修改tool

--- tools/msys64/msys64/ucrt64/bin/dbus-env.bat
+++
@@ -1,8 +0,0 @@
-:: environment setting for dbus clients
-@echo off
-
-:: session bus address
-set DBUS_SESSION_BUS_ADDRESS=autolaunch:
-
-:: system bus address
-set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=unix:path=/ucrt64/var/run/dbus/system_bus_socket
--- tools/msys64/msys64/ucrt64/lib/cmake/Qt5Core/Qt5CoreConfigExtrasMkspecDir.cmake
+++
@@ -1,2 +0,0 @@
-
-set(_qt5_corelib_extra_includes "${_qt5Core_install_prefix}/share/qt5//mkspecs/win32-g++")
