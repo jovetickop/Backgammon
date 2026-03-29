5b96e062922f7704d1f3492327b98acfefcb98df
jiangjie <1121271124@qq.com>
2026-03-29 20:54:41 +0800

    chore: 清理临时文件，更新 .gitignore

--- test-run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=C:\msys64\ucrt64\bin;%PATH%
-cd /d C:\code\Backgammon\build
-echo Starting... > ..\debug.log
-Backgammon.exe >> ..\debug.log 2>&1
-echo Done >> ..\debug.log
--- test-run2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=C:\msys64\ucrt64\bin;C:\Windows\System32;%PATH%
-cd /d C:\code\Backgammon\build
-Backgammon.exe 2> err.log
